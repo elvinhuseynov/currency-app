85c5ba4869f2ab930594d550bed09db404c2bbe1
elvin.huseinov <elvin.huseinov@cybernet.az>
2021-11-24 17:52:40 +0400

    initial

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry, Input, Layout, Button } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+export const ThemeContext = React.createContext({
+  theme: 'light',
+  toggleTheme: () => {},
+});
 
 export default function App() {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('dark');
+
+  const toggleTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva[theme]}>
+        <StatusBar style="auto" />
+        <Layout style={styles.container} >
+          <SafeAreaView>
+            <View>
+              <Input label="hello" style={{ width: 300 }} />
+              <Input label="mello" style={{ width: 300 }} />
+              <Button>hello</Button>
+            </View>
+          </SafeAreaView>
+        </Layout>
+      </ApplicationProvider>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+//     // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
